eb3f37fbc4617d64b6aa8b6511d6d8aa8f524ddf
moran <3575331055@qq.com>
2025-02-08 18:46:03 +0800

    feat：为reset_database等爬虫加入异步并发支持

--- NineBiliRank/database/__init__.py
+++ NineBiliRank/database/__init__.py
@@ -4,9 +4,9 @@ from config import get_config
 from .Base import Base
 
 config = get_config()
-
+task_count = config["basic_config"]["spyder"]["async"]["task_count"]
 # sync_engine = create_engine(config["basic_config"]["database"]["db_url"])
-async_engine = create_async_engine(config["basic_config"]["database"]["db_url"])
+async_engine = create_async_engine(config["basic_config"]["database"]["db_url"], pool_size=task_count, max_overflow=64)
 
 async_session = async_sessionmaker(async_engine)
 # session = sessionmaker(sync_engine)
